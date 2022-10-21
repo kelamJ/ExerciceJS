fd886105a3019766d5aa5cf9199b2673fb373491
Malek Julien <malekjulien80@gmail.com>
2022-10-21 09:53:33 +0200

    Exercice les multiples

--- Exercice/multiples.js
+++ Exercice/multiples.js
@@ -1,7 +1,8 @@
-let x = parseInt(prompt("Saisissez un nombre entier"));
+let x = parseInt(prompt("Saisissez un nombre que voulez multipliez"));
 let t = parseInt(prompt("Saisissez le nombre de multiple que vous voulez afficher"));
 let n = 0;
-for (n = 0; n < t; n++  )
+for (n = 1; n <= t; n++  )
 {
-    console.log(x+" x " +t +" = " (x*t))
+    resultat = x * t;
+    console.log( x +" x " + n + " = " + (x *n ))
 }
